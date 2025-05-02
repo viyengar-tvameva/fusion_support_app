5ccfb63b0fdadedc145577809ccddf1c4552bd9b
viyengar-tvameva <varada@tvameva.ai>
2025-05-02 01:08:16 -0500

    Update otel_config.py

--- config/otel_config.py
+++ config/otel_config.py
@@ -1,1 +1,23 @@
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
 
+resource = Resource.create(attributes={
+    "service.name": "fusion_support_app",
+    "service.version": "1.0.0"
+})
+
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer_provider = trace.get_tracer_provider()
+
+otlp_exporter = OTLPSpanExporter(endpoint="http://<SIGNOZ_HOST>:4317", insecure=True)
+span_processor = BatchSpanProcessor(otlp_exporter)
+tracer_provider.add_span_processor(span_processor)
+
+def configure_instrumentation(app):
+    FastAPIInstrumentor.instrument_app(app)
+    RequestsInstrumentor().instrument()
