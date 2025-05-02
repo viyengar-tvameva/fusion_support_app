26ffe753b92cb4c8d318b6022586e7c38a56336d
viyengar-tvameva <varada@tvameva.ai>
2025-05-02 04:25:57 -0500

    Update ticket_metrics.py

--- metrics/ticket_metrics.py
+++ metrics/ticket_metrics.py
@@ -1,1 +1,14 @@
+from prometheus_client import Counter
 
+tickets_created = Counter("tickets_created", "Number of tickets created")
+tickets_resolved = Counter("tickets_resolved", "Number of tickets resolved")
+tickets_escalated = Counter("tickets_escalated", "Number of tickets escalated")
+
+def increment_tickets_created():
+    tickets_created.inc()
+
+def increment_tickets_resolved():
+    tickets_resolved.inc()
+
+def increment_tickets_escalated():
+    tickets_escalated.inc()
